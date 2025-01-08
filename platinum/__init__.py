811fd88cf61abf6aa5b4142a8d41b791dd219436
Simon.McCurrach <simon.mccurrach@processsensing.com>
2025-01-08 13:45:33 +0000

    init updates for docs details

--- platinum/__init__.py
+++ platinum/__init__.py
@@ -4,4 +4,5 @@
 See `platinum.platinum_single_range` for main codebase to communicate with the sensor.
 
 See `platinum.utilities` for submodules used by the main code such as checksum creation.
+See `platinum.platinum_data_structures` for data structures used by the sensor versions and data types.
 """
